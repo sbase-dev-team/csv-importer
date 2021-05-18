b1d2f86ceb1dca7caaf5e68719091977fbe486ac
unknown <alphab1909@gmail.com>
2021-05-18 18:17:39 +0300

    Style changes

--- resources/js/components/UploadComponent.vue
+++ resources/js/components/UploadComponent.vue
@@ -1,37 +1,22 @@
 <template>
-    <div class="container">
+    <div class="container mt-2">
         <modal-mapping :headers="headers" :columns="columns" @process="importFile"></modal-mapping>
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">CSV Import</div>
-
+                    <div class="panel-heading mb-2">CSV Import</div>
                     <div class="panel-body">
                         <form class="form-horizontal" @submit="submitFile">
                             <div class="form-group">
-                                <label for="csv_file" class="col-md-4 control-label">CSV file to import</label>
-
-                                <div class="col-md-6">
-                                    <input id="csv_file" ref="file" @change="handleFileUpload()" type="file" class="form-control" name="csv_file" accept=".csv"required>
+                                <div class="custom-file">
+                                    <input  id="csv_file" ref="file" @change="handleFileUpload()" accept=".csv" type="file" name="csv_file" class="custom-file-input" required>
+                                    <label class="custom-file-label" for="csv_file">Choose file...</label>
                                 </div>
                             </div>
-
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="header" checked> File contains header row?
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary" :disabled="!file">
-                                        Parse CSV
-                                    </button>
-                                </div>
+                                <button type="submit" class="btn btn-primary" :disabled="!file">
+                                    Parse CSV
+                                </button>
                             </div>
                         </form>
                     </div>
